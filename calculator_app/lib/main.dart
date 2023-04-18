c73f9a7192318e9b3644732445f08a589aceab2f
Sujan Pandey <sujanpandey@Sujans-MacBook-Pro.local>
2023-04-18 13:59:19 +0545

    second commit

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:calculator_app/my_button.dart';
+import 'package:calculator_app/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,25 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData( 
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            children: [
-              Row(
-                children: const [
-                  MyButton(title: 'AC'),
-                  MyButton(title: '+/-' ),
-
-                  MyButton(title: '%' ),
-                  MyButton(title: '/', color: Color (0xffffa00a), ),
-                ],
-              )
-            ],
-          ),
-        ),
-      )),
+      home:  const HomeScreen()
     );
   }
 }
